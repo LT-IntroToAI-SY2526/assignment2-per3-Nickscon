261caff5e9a30fd85694ddf17f3960c28b2aee66
Nickscon <nzhang@cps.edu>
2025-09-23 10:28:41 -0500

    finished

--- a2.py
+++ a2.py
@@ -41,7 +41,17 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
                 print(result)
                 return result
             else:
-                pass #placehold
+                pind += 1
+                accum = ""
+                while pattern[pind] != source[sind]:
+                    accum += source[sind] + " "
+                    sind += 1
+
+                    if sind == len(source):
+                        return None
+                
+                result.append(accum.strip())
+
 
         # 3) if we reached the end of the source but not the pattern
         elif sind == len(source):
